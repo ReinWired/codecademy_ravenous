d2bbc0a7520dcbf7626c8810535cdefb5e776274
ReinWired <palmisanoandre@hotmail.com>
2020-02-01 00:03:25 +0100

    Add search simulation & Searchbar methods as event listeners

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -10,6 +10,9 @@ class SearchBar extends React.Component {
 			location: '',
 			sortBy: 'best_match'
 		};
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+		this.handleSearch = this.handleSearch.bind(this);
 		this.sortByOptions = {
 			"Best Match": "best_match",
 			"Highest Rated": "rating",
@@ -27,6 +30,19 @@ class SearchBar extends React.Component {
 	
 	handleSortByChange(sortByOption) {
 		this.setState({ sortBy: sortByOption });
+	}
+
+	handleTermChange(e) {
+		this.setState({ term: e.target.value });
+	}
+
+	handleLocationChange(e) {
+		this.setState({ location: e.target.value });
+	}
+
+	handleSearch(e) {
+		this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+		e.preventDefault();
 	}
 	
 	renderSortByOptions() {
@@ -47,11 +63,11 @@ class SearchBar extends React.Component {
 				</ul>
 			  </div>
 			  <div className="SearchBar-fields">
-				<input placeholder="Search Businesses" />
-				<input placeholder="Where?" />
+				<input placeholder="Search Businesses" onChange={this.handleTermChange} />
+				<input placeholder="Where?" onChange={this.handleLocationChange} />
 			  </div>
 			  <div className="SearchBar-submit">
-				<a>Let's Go</a>
+				<a onClick={this.handleSearch} >Let's Go</a>
 			  </div>
 			</div>
 		);
